29e67393febb26799eb1e01de79848ed2d79a03a
Roman Pozdnyakov <proklus@gmail.com>
2023-02-27 23:44:06 +0300

    almost done

--- interface_lib.py
+++ interface_lib.py
@@ -1,18 +1,34 @@
 import os
-#from time import sleep
+
 
 def clear_screen():
-
-# Printing the os name
-    print("os name is :", os.name)
-
-# Waiting for 2 seconds to clear the screen
-#    sleep(2)
-
-# Clearing the Screen
-# posix is os name for Linux or mac
-    if(os.name == 'posix'):
-     os.system('clear')
-# else screen will be cleared for windows
+    if (os.name == 'posix'):
+        os.system('clear')
     else:
-     os.system('cls')
+        os.system('cls')
+
+
+def print_element(elmt):
+    print(f"Имя: {elmt[0]}, {elmt[1]} {elmt[2]}")
+    print(f"Тел: {'  '.join(elmt[3])}")
+    print(f"e-mail: {'  '.join(elmt[4])}")
+    print(f"addr: {elmt[5]}")
+    print()
+
+
+def print_book(data):
+    for k, v in data.items():
+        print(f"#{k}")
+        print_element(v)
+        # print(f"Имя: {v[0]}, {v[1]} {v[2]}")
+        # print(f"Тел: {'  '.join(v[3])}")
+        # print(f"e-mail: {'  '.join(v[4])}")
+
+
+def find_element(data, element):
+    not_found = True
+    for k, v in data.items():
+        if element in v:
+            print_element(v)
+    if not_found:
+        print('Ничего не нашёл')
--- main.py
+++ main.py
@@ -1,49 +1,66 @@
-import os
 import JSON_format as jsons
 import interface_lib as iface
 
-# Data structure
-# phonebook = {id: [l_name, f_name, m_name, [phone1, phone2], [email1, email2], address]}
-#phonebook = {1: ['Smith', 'John', None, ['+1776849096'], ['abc@d.com']]}
-phonebook = jsons.load('PB_DB.json')
-print(phonebook)
-jsons.save('PD_DB.json', phonebook)
-# phonebook = [
-#  [12345, True, "яблоко", {"Миша": [898981646, 464654654]}],\
-#  [12345, True, "яблоко", {"Миша": [898981646, 464654654]}]\
-#  ]
-#print(*phonebook)
-#jsons.write_json_file('PB_DB.json', phonebook)
-1
-# phonebook = jsons.load('PB_DB.json')
-# print(phonebook)
-#
-# pb_record = {'surname': "Иванов", 'firstname': 'Иван', 'middlename': 'Иванович', 'address': '', 'email': [], 'phone': []\
-#              }
+# Data structure {id: ['l_name', 'f_name', 'm_name', ['phone'], ['email'], 'city']}
 
+phonebook = jsons.load('PD_DB.json')
+# del phonebook['id']
+print(f"Количество записей: {len(phonebook)}")
+print(max(phonebook.keys()))
 
-# read from file here
 flag = True
 while flag:
-    iface.clear_screen()
-    #os.system('clear')
-    print('Доступные опции:',\
-          '1 -> выводит список на экран',\
-          '2 -> добавление контакта',\
-          '3 -> поиск контакта',\
-          '0 -> выход (с сохранением)',\
+    # iface.clear_screen()
+    print('Доступные опции:', \
+          '1 -> вывести все контакты', \
+          '2 -> добавление контакта', \
+          '3 -> поиск контакта', \
+          '4 -> изменить контакт', \
+          '5 -> удалить контакт', \
+          '0 -> выход (с сохранением)', \
           sep='\n')
 
     user_input = input('> ').lower()
     match user_input:
         case "1":
             print('Отображение списка.')
+            iface.print_book(phonebook)
         case "2":
             print('Добавление контакта')
             # блок ВВОДА
+            next_id = int(max(phonebook.keys())) + 1
+            ph_record = input('Введите ФИО через пробел (порядок важен): ').split()
+            if len(ph_record) < 3:
+                for i in range(3 - len(ph_record)):
+                    ph_record.append('')
+            ph_record.append(
+                input('Введите номера телефонов через пробел (в самом номере не должно быть пробелов): ').split())
+            ph_record.append(
+                input('Введите email адреса через пробел: ').split())
+            ph_record.append(input('Введите адрес: '))
+            print(ph_record)
+            phonebook[next_id] = ph_record
         case "3":
             print('Поиск контакта')
+            print(
+                'Поиск проводится по любому полю справочника, по полному или частичному совпадению (Чувствительно к регистру). '
+                'Задайте строку поиска. Если хотите найти по номеру записи, введите #n (n - номер записи).')
+            string_to_find = input('> ')
+            if string_to_find.startswith('#'):
+                iface.print_element(phonebook[string_to_find[1:]])
+            else:
+                iface.find_element(phonebook, string_to_find)
+        case "4":
+            print('Изменить контакт')
+            # Change entry here
+            # string_to_find = input('Что ищем? ')
+            # iface.find_element(phonebook, string_to_find)
+        case "5":
+            print('Удалить контакт')
+            # Delete entry here
+            # string_to_find = input('Что ищем? ')
+            # iface.find_element(phonebook, string_to_find)
         case "0":
             print('quiting')
-            # save to file here
+            jsons.save('PD_DB.json', phonebook)
             flag = False
